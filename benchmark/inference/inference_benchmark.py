be850b0ef6eb210c02e44107152abbc6862f4b5d
Jakub Pietrak <97102979+JakubPietrakIntel@users.noreply.github.com>
2022-10-06 14:45:41 +0200

    Update inference_benchmark.py
    inputs for full benchmark

--- benchmark/inference/inference_benchmark.py
+++ benchmark/inference/inference_benchmark.py
@@ -22,7 +22,7 @@ WARMUP = 1
 BATCH_SIZE = 512
 NUM_HIDDEN_CHANNELS = [64]
 NUM_LAYERS = [2]
-HETERO_NEIGHBORS = [3, 5]
+HETERO_NEIGHBORS = [5, 10]
 
 def run(args: argparse.ArgumentParser) -> None:
 
@@ -55,7 +55,7 @@ def run(args: argparse.ArgumentParser) -> None:
                         f'not supported. Skipping.')
                 continue
             
-            for num_workers in [0,1]:#,2,3,4,8,12,16,20,24,30,36,44,56]:
+            for num_workers in [0,1,2,3,4,8,12,16,20,24,30,36,44,56]:
                 for cpu_affinity in [True, False]:
                     if num_workers == 0 and cpu_affinity:
                         continue
