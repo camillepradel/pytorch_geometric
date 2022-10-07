9061fa0cf0e69aa997d2a554f0b211af809b3ea9
Jakub Pietrak <97102979+JakubPietrakIntel@users.noreply.github.com>
2022-10-07 14:57:39 +0200

    Update inference_benchmark.py
    fix ranges

--- benchmark/inference/inference_benchmark.py
+++ benchmark/inference/inference_benchmark.py
@@ -58,7 +58,7 @@ def run(args: argparse.ArgumentParser) -> None:
                         f'not supported. Skipping.')
                 continue
             try:
-                for num_workers in [0,1,2,3,4,8,12,16,20,24,30,36,44,56]:
+                for num_workers in [0,1,2,3,4,8,12,16,20,24,30,30,40,56]:
                     for cpu_affinity in [True, False]:
                         if num_workers == 0 and cpu_affinity:
                             continue
