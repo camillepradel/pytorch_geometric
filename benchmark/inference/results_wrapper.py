c63a9beed8571d90793ee73b06d51cc0f97db24a
Jakub Pietrak <jakub.pietrak@intel.com>
2022-10-14 14:02:06 +0200

    wrapper improvements

--- benchmark/inference/results_wrapper.py
+++ benchmark/inference/results_wrapper.py
@@ -20,6 +20,7 @@ def analyse(platform) -> None:
         if ".log" in file:
             filedir=f"{LOGS}/{file}"
             model = file.split('_')[0].strip()
+            #config=file.replace('_','')[:-4]
             config = file.split('_')[1][:-4].strip()
             test_result[0] = model
             test_result[1] = SUPPORTED_SETS.get(model, None)
@@ -40,16 +41,26 @@ def plot(platform):
     data['setup'] = np.nan 
     models = ['gcn','gat','rgcn']
     datasets = ['Reddit', 'Reddit', 'ogbn-mag']
+    configs = {'init':[3,512,2,16,0],
+                'fin':[5,512,3,256,0]}
+    cfg_items = ["<br>num_neighbors=","batch_size=","num_layers=","hidden_channels","warmup="]    
+    machines = {'SPR':"2xSPR + 256GB RAM",
+                'ICX':"2xICX + 512GB RAM",
+                'CSX':"2xCSX + 256GB RAM"}
+    
+    version = platform.split('-')[1]
+    platform = platform.split('-')[0]
+    
+    cfg_val= configs.get(version, None)
+    
     for i, model in enumerate(models):
-        dataset = datasets[i]
-        machines = {'SPR':"2xSPR + 256GB RAM",
-                    'ICX':"2xICX + 512GB RAM",
-                    'CSX':"2xCSX + 256GB RAM"}
+        # str formatting
         title = machines.get(platform, None)
-        title += f"<br>{model}+{dataset}"
-        cfg = "<br>num_neighbors=[-1], " if model!='rgcn' else "<br>num_neighbors=[3,3], "
-        cfg += "batch_size=512, num_layers=2, hidden_channels=16, warmup=0"
-        title = title + cfg
+        title += f"<br>{model}+{datasets[i]}<br>" 
+        cfg = f"num_neighbors={[-1] * cfg_val[2]}, " if model !='rgcn' else f"num_neighbors={[cfg_val[0]] * cfg_val[2]}, "
+        cfg += ''.join([f"{k}{val}, " for k, val in zip(cfg_items[1:], cfg_val[1:])])
+        title += cfg[:-2]
+        # plot data
         model_data = model_mask(data, model)
         fig = px.line(model_data, x = "NR_WORKERS", y = "TIME(s)", color = 'setup', 
                         height = 500, width = 1000,
@@ -61,9 +72,9 @@ def plot(platform):
         fig.update_xaxes(type = 'category', categoryarray=np.unique(model_data["NR_WORKERS"]))
         avg_time_aff = model_data[(model_data['setup'] == 'NO_HT+AFF') | (model_data['setup'] == 'HT+AFF')].mean()
         avg_time_aff = round(avg_time_aff['TIME(s)'],2)
-        avg_time_noaff = model_data[(model_data['setup'] == 'NO_HT+NO_AFF') | (model_data['setup'] == 'HT+NO_AFF')].mean()
+        avg_time_noaff = model_data[(model_data['setup'] == 'NO_HT+NO_AFF') | (model_data['setup'] == 'HT+NO_AFF')].iloc[1:].mean()
         avg_time_noaff = round(avg_time_noaff['TIME(s)'],2)
-        fig.add_annotation(text=f'Avg. time NO_AFF: {avg_time_noaff}s<br>Avg. time AFF: {avg_time_aff}s', 
+        fig.add_annotation(text=f"Avg. time NO_AFF*: {avg_time_noaff}s<br>Avg. time AFF: {avg_time_aff}s<br><br>* excl. nr_workers = 0", 
                     align='left',
                     showarrow=False,
                     xref='paper',
@@ -72,7 +83,7 @@ def plot(platform):
                     y=0.5,
                     bordercolor='white',
                     borderwidth=1)
-        fig.write_image(f"{PLOTS}/{platform}-{model}.png")
+        fig.write_image(f"{PLOTS}/{platform}-{version}-{model}.png")
          
 def model_mask(data, model):
     
@@ -87,9 +98,9 @@ def model_mask(data, model):
     
 if __name__ == '__main__':
     
-    platform = "SPR"
+    platform = "SPR-init"
     
-    CWD=f'pytorch_geometric/benchmark/inference/logs/test/{platform}'
+    CWD=f'pytorch_geometric/benchmark/inference/logs/{platform}'
     LOGS=f"{CWD}/logs"
     SUMMARY=f"{CWD}/summary_{platform}.csv"
     PLOTS=f'{CWD}/plots'
